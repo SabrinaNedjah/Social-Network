721ff3f956c668020c26072c34df4665db2f1c32
Sabrina nedjah <snedjah78@gmail.com>
2016-04-30 22:46:33 +0200

    new

---
+++ includes/constant.php
@@ -0,0 +1,3 @@
+<?php
+ //Constante qui definit le nom du site
+define('WEBSITE_NAME', 'Social Nework');
---
+++ register.php
@@ -0,0 +1,12 @@
+<?php
+    if(isset($_POST['register']){
+        
+    }
+
+
+?>
+
+
+
+
+<?php require('views/register.view.php'); ?>
